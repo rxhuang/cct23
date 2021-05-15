15344fba6a8d5c3f89819e8b669992e88ceb6044
Ruoxin Huang <rxhuang@live.ca>
2021-05-15 02:12:18 -0700

    Add files via upload

--- t2.py
+++ t2.py
@@ -8,19 +8,19 @@ import subprocess
 import os
 
 
+def main(event: func.EventGridEvent, context: func.Context): 
 
-def main(event: func.EventGridEvent, context: func.Context):   
-
-    url = os.environ.get('VIDEO_STORAGE_ACCOUNT_URL')
+    ur = os.environ.get('VIDEO_STORAGE_ACCOUNT_URL')
     key = os.environ.get('VIDEO_STORAGE_ACCOUNT_API_KEY')
 
-    name = event.subject.split('/')[-1].split('.')[0]
-    logging.info('Python HTTP trigger function processed a request with name: ' + name)
+    longname = event.subject.split('/')[-1]
+    name = longname.split('.')[0]
 
     if name:
-        blob_service_client = BlobServiceClient(account_url=url, credential=key)
-        blob_client = blob_service_client.get_blob_client('video-input-container',name+'.mov')
-        
+        blob_service_client = BlobServiceClient(account_url=ur, credential=key)
+        blob_client = blob_service_client.
+                        get_blob_client('video-input-container', longname)
+
         video_path = '/tmp/'
         video_file = os.path.join(video_path, name)
         thumbnail_path = '/tmp/thumbnails/'
@@ -34,17 +34,19 @@ def main(event: func.EventGridEvent, context: func.Context):
         with open(video_file, "wb") as f:
             download_stream = blob_client.download_blob()
             f.write(download_stream.readall())
-        
+
         subprocess.call(['chmod', '777', 'ffmpeg'])
-        ff = FFmpeg(executable= ff_exe, inputs={video_file: None}, outputs={thumbnail_path+name+'_%d.png': '-y -vf "fps=1"'})
+        ff = FFmpeg(executable=ff_exe, inputs={video_file: None}, 
+                    outputs={thumbnail_path+name+'_%d.png': '-y -vf "fps=1"'})
         ff.run()
 
         for filename in os.listdir(thumbnail_path):
             if name in filename:
                 with open(os.path.join(thumbnail_path, filename), 'rb') as f:
-                    blob_client = blob_service_client.get_blob_client('thumbnail-container', filename)
+                    blob_client = blob_service_client
+                                    .get_blob_client('thumbnail-container', filename)
                     blob_client.upload_blob(f, blob_type="BlockBlob")
-        
+
         logging.info('Success')
 
     logging.info('End of request')
