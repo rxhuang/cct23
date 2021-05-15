4de69ea931d7403165c3d4969f6c5236a2e67461
Ruoxin Huang <rxhuang@live.ca>
2021-05-15 02:34:00 -0700

    Add files via upload

--- t2.py
+++ t2.py
@@ -8,7 +8,7 @@ import subprocess
 import os
 
 
-def main(event: func.EventGridEvent, context: func.Context): 
+def main(event: func.EventGridEvent, context: func.Context):
 
     ur = os.environ.get('VIDEO_STORAGE_ACCOUNT_URL')
     key = os.environ.get('VIDEO_STORAGE_ACCOUNT_API_KEY')
@@ -18,8 +18,8 @@ def main(event: func.EventGridEvent, context: func.Context):
 
     if name:
         blob_service_client = BlobServiceClient(account_url=ur, credential=key)
-        blob_client = \
-            blob_service_client.get_blob_client('video-input-container', longname)
+        blob_client = blob_service_client.get_blob_client\
+            ('video-input-container', longname)
 
         video_path = '/tmp/'
         video_file = os.path.join(video_path, name)
@@ -36,14 +36,14 @@ def main(event: func.EventGridEvent, context: func.Context):
             f.write(download_stream.readall())
 
         subprocess.call(['chmod', '777', 'ffmpeg'])
-        ff = FFmpeg(executable=ff_exe, inputs={video_file: None}, 
+        ff = FFmpeg(executable=ff_exe, inputs={video_file: None},
                     outputs={thumbnail_path+name+'_%d.png': '-y -vf "fps=1"'})
         ff.run()
 
         for filename in os.listdir(thumbnail_path):
             if name in filename:
                 with open(os.path.join(thumbnail_path, filename), 'rb') as f:
-                    blob_client = blob_service_client.get_blob_client \
+                    blob_client = blob_service_client.get_blob_client\
                         ('thumbnail-container', filename)
                     blob_client.upload_blob(f, blob_type="BlockBlob")
 
